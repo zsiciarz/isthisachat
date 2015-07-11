49505f15b68590efe4a30e84919f40efdbbbae47
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-07-11 23:33:51 +0200

    Broadcast join message.

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var WebSocketServer = require('ws').Server;
 connect().use(serveStatic(__dirname)).listen(8080);
 
 var wss = new WebSocketServer({port: 6639});
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
+wss.broadcast = function (data) {
+  wss.clients.forEach(function (client) {
     client.send(data);
   });
 };
 
 wss.on('connection', function (ws) {
+  wss.broadcast('Somebody joined!');
   ws.on('message', function (message) {
     wss.broadcast(message);
   });
