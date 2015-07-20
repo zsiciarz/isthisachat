f5af633e7f7345a3c67ea04b2187857a54d3af52
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-07-20 16:56:05 +0200

    First attempt at ES6.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var socket = new WebSocket("ws://127.0.0.1:6639");
-    var chat = document.getElementById('chat');
-    var chatInput = document.getElementById('chat-input');
-    var message = document.getElementById('message');
-    chatInput.addEventListener('submit', function (e) {
+document.addEventListener('DOMContentLoaded', () => {
+    let socket = new WebSocket("ws://127.0.0.1:6639");
+    let chat = document.getElementById('chat');
+    let chatInput = document.getElementById('chat-input');
+    let message = document.getElementById('message');
+    chatInput.addEventListener('submit', e => {
         socket.send(message.value);
         message.value = '';
         e.preventDefault();
     });
-    socket.onmessage = function (event) {
-        var li = document.createElement('li');
+    socket.onmessage = event => {
+        let li = document.createElement('li');
         li.textContent = event.data;
         chat.appendChild(li);
         li.scrollIntoView(false);
