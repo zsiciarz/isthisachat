f34aad3d4f52c1df69958f2e34655e89b105ed4e
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-07-21 13:15:05 +0200

    Fixed path given to serveStatic after move.

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,10 @@
 let connect = require('connect');
 let faker = require('faker');
+let path = require('path');
 let serveStatic = require('serve-static');
 let WebSocketServer = require('ws').Server;
 
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(path.join(__dirname, '../'))).listen(8080);
 
 let wss = new WebSocketServer({port: 6639});
 wss.broadcast = data => {
