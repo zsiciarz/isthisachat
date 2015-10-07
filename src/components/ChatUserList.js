e9c9153c82186cdd333dbd0a87aec25fee6f2631
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-10-07 20:33:24 +0200

    Key user list by nicks.

--- src/components/ChatUserList.js
+++ src/components/ChatUserList.js
@@ -4,7 +4,7 @@ export class ChatUserList extends React.Component {
     render () {
         return (
             <ul>
-                {this.props.nicks.map(nick => <li>{nick}</li>)}
+                {this.props.nicks.map(nick => <li key={nick}>{nick}</li>)}
             </ul>
         );
     }
